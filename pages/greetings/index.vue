4ca69b889d15ad6c32c40d2fbc4e4defae04467a
Dalton Saffe <daltonsaffe@gmail.com>
2022-01-29 00:49:06 -0500

    add 2021 photos

---
+++ pages/greetings/index.vue
@@ -0,0 +1,371 @@
+<template>
+  <section class="container">
+    <h1 class="title-right">we miss you</h1>
+    <div class="logo-wrap">
+      <Logo height="50" />
+    </div>
+    <p>
+      hey! if you’re receiving this card we are grateful to have you in our
+      lives and wanted to take a moment to send you a hello. We compiled music
+      and special moments from this past year to share with you and we hope they
+      delight you.
+    </p>
+    <p>
+      2021 was epic! We moved in together & returned to dance floors again as
+      vaccines rolled out. There were trips to Colorado & Miami to visit family
+      and lots of outdoor & musical adventures. We started documenting the
+      musical outpouring of us and our friends & throwing listening parties.
+      Click the rotating smiley above to explore our site and if you’d like to
+      join the newsletter drop your email below. Take care of yourself & keep
+      dancing :)
+    </p>
+    <p class="center">&lt;3</p>
+    <p class="center">L&amp;D</p>
+    <div id="mc_embed_signup">
+      <form
+        action="https://cafe.us7.list-manage.com/subscribe/post?u=7ac7e285f8d8826591ef315a7&amp;id=47d255f050"
+        method="post"
+        id="mc-embedded-subscribe-form"
+        name="mc-embedded-subscribe-form"
+        class="validate"
+        target="_blank"
+        novalidate
+      >
+        <div id="mc_embed_signup_scroll">
+          <div class="mc-field-group">
+            <input
+              type="email"
+              value=""
+              name="EMAIL"
+              class="email"
+              id="mce-EMAIL"
+            />
+          </div>
+          <div id="mce-responses" class="clear">
+            <div
+              class="response"
+              id="mce-error-response"
+              style="display: none"
+            ></div>
+            <div
+              class="response"
+              id="mce-success-response"
+              style="display: none"
+            ></div>
+          </div>
+          <div style="position: absolute; left: -5000px" aria-hidden="true">
+            <input
+              type="text"
+              name="b_7ac7e285f8d8826591ef315a7_47d255f050"
+              tabindex="-1"
+              value=""
+            />
+          </div>
+          <div class="clear">
+            <input
+              type="submit"
+              value="⮕"
+              name="subscribe"
+              id="mc-embedded-subscribe"
+              class="button"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+    <h1>2021</h1>
+    <iframe
+      width="100%"
+      height="166"
+      scrolling="no"
+      frameborder="no"
+      allow="autoplay"
+      src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/953332642&color=%232c2c2c&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false"
+    ></iframe>
+    <ul>
+      <li>tracklist</li>
+      <li>tracklist</li>
+      <li>tracklist</li>
+      <li>tracklist</li>
+    </ul>
+    <section class="month-cell">
+      <h2>01 - january</h2>
+      <img src="~/assets/2021/01/000113480003 2.jpg" />
+      <img src="~/assets/2021/01/000113480014 2.jpg" />
+      <img src="~/assets/2021/01/000113480025 2.jpg" />
+      <img src="~/assets/2021/01/000113480031 2.jpg" />
+      <img src="~/assets/2021/01/IMG_4981.jpeg" />
+    </section>
+    <section class="month-cell">
+      <h2>02 - february</h2>
+      <img src="~/assets/2021/02/000113470002 2.jpg" />
+      <img src="~/assets/2021/02/000568670035 2.jpg" />
+      <img src="~/assets/2021/02/000568670037 2.jpg" />
+      <img src="~/assets/2021/02/IMG_3377.jpg" />
+    </section>
+    <section class="month-cell">
+      <h2>03 - march</h2>
+      <img src="~/assets/2021/03/000113460023 2.jpg" />
+      <img src="~/assets/2021/03/000113460034.jpg" />
+      <img src="~/assets/2021/03/IMG_3387 2.jpg" />
+      <img src="~/assets/2021/03/IMG_3388.JPG" />
+      <img src="~/assets/2021/03/IMG_3331.jpg" />
+      <img src="~/assets/2021/03/IMG_3335.jpg" />
+      <img src="~/assets/2021/03/IMG_3396 2.jpg" />
+      <img src="~/assets/2021/03/IMG_4787.jpg" />
+    </section>
+    <section class="month-cell">
+      <h2>04 - april</h2>
+      <img src="~/assets/2021/04/000128390020 2.jpg" />
+      <img src="~/assets/2021/04/000128390023 2.jpg" />
+      <img src="~/assets/2021/04/000128390026 2.jpg" />
+      <img src="~/assets/2021/04/000128420004 2 2.jpg" />
+      <img src="~/assets/2021/04/000128420006 2 2.jpg" />
+      <img src="~/assets/2021/04/000128420008 2 2.jpg" />
+      <img src="~/assets/2021/04/Scan 1.jpeg" />
+      <img src="~/assets/2021/04/Scan 2.jpeg" />
+      <img src="~/assets/2021/04/Scan 8.jpeg" />
+    </section>
+    <section class="month-cell">
+      <h2>05 - may</h2>
+      <img src="~/assets/2021/05/000128400001 2 2.jpg" />
+      <img src="~/assets/2021/05/000128400022 2 2.jpg" />
+      <img src="~/assets/2021/05/000128400025 2 2.jpg" />
+      <img src="~/assets/2021/05/000128400029 2 2.jpg" />
+      <img src="~/assets/2021/05/000128420030 2 2.jpg" />
+      <img src="~/assets/2021/05/Scan 3.jpeg" />
+      <img src="~/assets/2021/05/Scan 4.jpeg" />
+      <img src="~/assets/2021/05/Scan.jpeg" />
+    </section>
+    <section class="month-cell">
+      <h2>06 - june</h2>
+      <img src="~/assets/2021/06/IMG_8502.jpg" />
+      <img src="~/assets/2021/06/000310020021 3 2.jpg" />
+      <img
+        src="~/assets/2021/06/64434090908__E7C4955F-F251-4C00-B0EC-F05FC8522B1C.jpg"
+      />
+      <img src="~/assets/2021/06/IMG_0026.jpeg" />
+      <img src="~/assets/2021/06/IMG_3603 2.jpg" />
+      <img src="~/assets/2021/06/IMG_3632 2.jpg" />
+      <img src="~/assets/2021/06/IMG_7775.jpg" />
+    </section>
+    <section class="month-cell">
+      <h2>07 - july</h2>
+      <img src="~/assets/2021/07/000309990022 2 2.jpg" />
+      <img src="~/assets/2021/07/000310010021 2 2.jpg" />
+      <img src="~/assets/2021/07/000310010013 2 2.jpg" />
+      <img src="~/assets/2021/07/000310000004 2 2.jpg" />
+      <img src="~/assets/2021/07/000310000007 2 2.jpg" />
+      <img src="~/assets/2021/07/000310000021 2 2.jpg" />
+      <img src="~/assets/2021/07/000310030015 2 2.jpg" />
+      <img src="~/assets/2021/07/000310040001 2 2.jpg" />
+      <img src="~/assets/2021/07/000310040034 2 2.jpg" />
+      <img src="~/assets/2021/07/IMG_0739.jpg" />
+      <img src="~/assets/2021/07/IMG_3734.jpg" />
+      <img src="~/assets/2021/07/Scan 6.jpeg" />
+      <img src="~/assets/2021/07/Scan 7.jpeg" />
+      <img src="~/assets/2021/07/Scan 9.jpeg" />
+      <img src="~/assets/2021/07/Scan 10.jpeg" />
+      <img src="~/assets/2021/07/Scan 11.jpeg" />
+    </section>
+    <section class="month-cell">
+      <h2>08 - august</h2>
+      <img src="~/assets/2021/08/000558110008 2.jpg" />
+      <img src="~/assets/2021/08/000558110034 2.jpg" />
+      <img src="~/assets/2021/08/IMG_1836.jpg" />
+      <img src="~/assets/2021/08/IMG_2826.jpg" />
+      <img src="~/assets/2021/08/IMG_3760.jpg" />
+    </section>
+    <section class="month-cell">
+      <h2>09 - september</h2>
+      <img src="~/assets/2021/09/Scan 5.jpeg" />
+      <img src="~/assets/2021/09/D&L.jpg" />
+      <img src="~/assets/2021/09/Adi.jpg" />
+      <img src="~/assets/2021/09/FullSizeRender.jpg" />
+      <img src="~/assets/2021/09/IrvAdam.jpg" />
+      <img src="~/assets/2021/09/Jelani.jpg" />
+      <img src="~/assets/2021/09/Lauren.jpg" />
+      <img src="~/assets/2021/09/IMG_0061.jpg" />
+      <img src="~/assets/2021/09/IMG_2581.jpg" />
+      <img src="~/assets/2021/09/IMG_3517.jpg" />
+      <img src="~/assets/2021/09/IMG_3872.jpg" />
+      <img src="~/assets/2021/09/IMG_3898.jpg" />
+      <img src="~/assets/2021/09/IMG_3978.jpg" />
+      <img src="~/assets/2021/09/IMG_7584.jpg" />
+    </section>
+    <section class="month-cell">
+      <h2>10 - october</h2>
+      <img src="~/assets/2021/10/IMG_0045.jpg" />
+      <img src="~/assets/2021/10/IMG_4381.jpg" />
+      <img src="~/assets/2021/10/IMG_4057.jpg" />
+      <img src="~/assets/2021/10/IMG_4893.jpg" />
+    </section>
+    <section class="month-cell">
+      <h2>11 - november</h2>
+      <img
+        src="~/assets/2021/11/65862925362__B35766C6-C89D-4DF4-95D7-A2971A3355D8.jpg"
+      />
+      <img src="~/assets/2021/11/IMG_4072.jpg" />
+      <img src="~/assets/2021/11/IMG_5484.jpg" />
+      <img src="~/assets/2021/11/IMG_5486.jpg" />
+      <img src="~/assets/2021/11/IMG_5490.jpg" />
+      <img src="~/assets/2021/11/IMG_5503.jpg" />
+      <img src="~/assets/2021/11/IMG_5635.jpg" />
+    </section>
+    <section class="month-cell">
+      <h2>12 - december</h2>
+      <img src="~/assets/2021/12/IMG_5717.jpg" />
+      <img src="~/assets/2021/12/IMG_6077.jpg" />
+      <img src="~/assets/2021/12/IMG_6205.jpg" />
+      <img src="~/assets/2021/12/IMG_6226.jpg" />
+      <img src="~/assets/2021/12/Scan 12.jpeg" />
+      <img src="~/assets/2021/12/Scan 13.jpeg" />
+    </section>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.logo-wrap {
+  margin: 10px auto 0;
+}
+.title-right {
+  text-align: right;
+  margin: 20px 0 0;
+}
+p {
+  margin: 10px 0;
+  text-align: left;
+}
+.center {
+  text-align: center;
+}
+a {
+  color: #fff;
+  text-decoration: none;
+}
+a:hover {
+  text-decoration: underline;
+}
+.logo-wrap {
+  margin: 5% auto 0;
+}
+.media-center {
+  text-align: center;
+}
+iframe {
+  margin: 10px 0 20px;
+  opacity: 0.7;
+}
+.container {
+  display: flex;
+  max-width: 700px;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  flex-direction: column;
+  font-size: 1.25rem;
+  background-color: var(--black);
+}
+.month-cell {
+  margin: 5% 0;
+}
+.month-cell img,
+.month-cell video {
+  width: 100%;
+  height: auto;
+  margin: 5% 0;
+}
+.month-cell p:first-of-type {
+  margin-right: 0;
+  margin: 2% 0;
+}
+.month-cell h2 {
+  margin: 3% 0;
+  font-size: 1.75rem;
+}
+h1 {
+  font-size: 2.25rem;
+}
+.add-top {
+  margin-top: 30px;
+}
+.month-cell p {
+  margin: 2% 10% 2% 0;
+  font-size: 1.25rem;
+}
+.email {
+  color: #fff;
+  border: 1px solid #fff;
+  background-image: none;
+  background-color: transparent;
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+  box-shadow: none;
+  font-size: 20px;
+  padding: 10px;
+  font-family: inherit;
+  width: 100%;
+}
+.button {
+  display: inline-block;
+  border: 1px solid #fff;
+  padding: 1rem;
+  width: 100%;
+  font-size: 2rem;
+  text-decoration: none;
+  background: transparent;
+  color: #ffffff;
+  font-family: sans-serif;
+  font-size: 1rem;
+  cursor: pointer;
+  text-align: center;
+  transition: background 250ms ease-in-out, transform 150ms ease;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  margin-left: 10px;
+}
+ul {
+  margin: 0;
+  padding: 0;
+}
+li {
+  margin: 10px 0;
+  font-size: 1rem;
+}
+.button:focus {
+  outline: 1px solid #fff;
+  outline-offset: -4px;
+}
+
+button:active {
+  transform: scale(0.99);
+}
+#mc_embed_signup {
+  margin: 50px 0;
+  width: 100%;
+}
+#mc_embed_signup_scroll {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+@media only screen and (max-width: 850px) {
+  .container {
+    margin: 0 auto;
+    max-width: 400px;
+  }
+}
+@media only screen and (max-width: 500px) {
+  .container {
+    margin: 0 10%;
+  }
+}
+</style>
