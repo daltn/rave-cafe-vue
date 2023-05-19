77670809caba91491f8ffc25ae1e9112e6644df5
Dalton Saffe <daltonsaffe@gmail.com>
2023-05-18 20:45:35 -0400

    add zine 002 pg

---
+++ pages/menu/002.vue
@@ -0,0 +1,638 @@
+<template>
+  <section class="menu-content">
+    <h1 class="mu-title">menu <sup>002</sup></h1>
+    [[[zine stack pic here]]]
+    <section class="menu-intro">
+      <p>Welcome to the digital component of our second zine :)</p>
+    </section>
+    <section class="player">
+      <iframe
+        width="100%"
+        height="300"
+        scrolling="no"
+        frameborder="no"
+        allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1622394739&color=%23ec6ca4&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true"
+      ></iframe>
+    </section>
+    <section class="essay">
+      <h3>The Positive-Sum Manifesto</h3>
+      <p class="center">by n3wb</p>
+      <p>
+        There are countless interactions and transactions we make with the
+        modern world around us each day that involve an inherent tension between
+        our wants and needs, and those of others. Underneath each of these
+        moments are social contracts we willingly accept, sometimes begrudgingly
+        when they are exploitative in nature. Our technology, our food, our
+        relationships, and our work are just a few examples of domains where we
+        make daily negotiations.
+      </p>
+      <p>
+        Humans have slowly evolved over hundreds of thousands of years, while
+        new technologies are introduced and innovated upon every day. While each
+        aspect of our modern lives can cause us harm if we stay in autopilot, we
+        col- lectively have the power to make positive-sum decisions that
+        benefit both ourselves and the world around us.
+      </p>
+      <p>
+        Within the broader principles of game theory are zero-sum games whereby,
+        as the dictionary defines it, “positive sum occurs when resources are
+        somehow increased and an approach is formulated in which the desires and
+        needs of all concerned are satisfied.” Whereas zero-sum games involve
+        one party benefiting from the other party's loss, positive-sum games
+        revolve around the collective good of both players.
+      </p>
+      <p>
+        The pandemic woke us up to the realities of the systems around us that
+        create zero-sum dynamics. Our modern healthcare system is optimized
+        around a “fee for service” model that is divorced from health outcomes.
+        Our agricultural and food systems are designed around cheap abundance
+        that ultimately makes us sicker. Our social networks and mobile devices
+        are designed to keep us continuously plugged-in, which ultimately has
+        led to an increase in loneliness and anxiety.
+      </p>
+      <p>
+        If we wait for these systems to redesign themselves as positive-sum
+        regenerative fly- wheels, we'll quickly deplete ourselves and the
+        resources around us. Instead, we must remain constantly alert and
+        vigilant, engaging only in activities that nourish our bodies and souls,
+        leaving us better off than we were yesterday. The second-order effects
+        of everything must be considered from the tap of a phone to the meat on
+        our plate. It may feel like a fight at first, but once we all experience
+        the magic of positive-sum games, we'll keep playing them indefinitely.
+      </p>
+      <p>
+        <strong> If you can read this, you are part of the resistance. </strong>
+      </p>
+    </section>
+    <section>
+      <img
+        src="https://ravecafe.s3.amazonaws.com/menu-002/anne-h.jpg"
+        alt="shore photo"
+      />
+      <section class="center">
+        <p>
+          <b>Where did it all go?</b><br />
+          Anne Hollowday <br />
+          <a
+            href="https://anne.holiday"
+            target="_blank"
+            rel="norefferer nofollow"
+            >anne.holiday</a
+          ><br />
+        </p>
+        <br />
+        <p>
+          Coastlines defy measurement, their slipperiness a result of their
+          fractal-like nature. But in parts of the UK, the immense coastline
+          faces the existential threat of erosion. As the sea chips away at
+          great British seaside traditions, we're left to ask the question, what
+          do we want to protect?
+        </p>
+      </section>
+    </section>
+    <section>
+      <img
+        src="https://ravecafe.s3.amazonaws.com/menu-002/bodega.jpg"
+        alt="people's bodega"
+      />
+      <p class="center">
+        <a
+          href="https://givebutter.com/peoplesbodeganyc"
+          target="_blank"
+          rel="norefferer nofollow"
+          >Support The People's Bodega</a
+        >
+      </p>
+    </section>
+    <section>
+      <img
+        src="https://ravecafe.s3.amazonaws.com/menu-002/body-menu002-levels.jpg"
+        alt="good morning rave cafe"
+      />
+    </section>
+    <iframe
+      style="border-radius: 12px"
+      src="https://open.spotify.com/embed/playlist/2by8XqpCINuRIc60a0oAyP?utm_source=generator"
+      width="100%"
+      height="352"
+      frameBorder="0"
+      allowfullscreen=""
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"
+    ></iframe>
+    <br />
+    <br />
+    <img
+      src="https://ravecafe.s3.amazonaws.com/menu-002/techno-trivia.jpg"
+      alt="techno trivia"
+    />
+    <a
+      href="https://drive.google.com/file/d/1KtYmaVBMdtnrUon0-XHF-PKBv9oHn_vo/view"
+      target="_blank"
+      rel="norefferer nofollow"
+      >Techno Trivia Answers</a
+    >
+    <section class="center">
+      <img
+        src="https://ravecafe.s3.amazonaws.com/menu-002/miss-alicia.jpg"
+        alt="miss alicia"
+      />
+
+      <p>
+        <b>Moment of Zen</b>
+        <br />
+        Miss Alicia
+      </p>
+      <p>It's good for looking, feeling, pondering, and taking a long walk</p>
+    </section>
+
+    <section class="menu-card">
+      <h2>Dish Soap</h2>
+      <section class="menu-card-section">
+        <b>Ingredients</b>
+        <ul>
+          <li>2 cups Warm (not hot) Filtered Water</li>
+          <li>1 tbsp Kosher Salt</li>
+          <li>1 cup Dr. Bronner's Sal Suds</li>
+          <li>Juice of one Lemon</li>
+          <li>1/4 tsp Vitamin E Oil</li>
+          <li>1 dropper (or 1/4 tsp) Grapefruit Seed Extract</li>
+          <li>2 tbsp Glycerin</li>
+          <li>10-20 drops Essential Oil of choice</li>
+        </ul>
+      </section>
+      <section class="menu-card-section">
+        <b>Directions</b>
+        <ul>
+          <li>Dissolve kosher salt in warm water</li>
+          <li>Add the rest of the ingredients</li>
+          <li>Whisk slowly until combined</li>
+        </ul>
+      </section>
+    </section>
+
+    <section class="menu-card">
+      <h2>Laundry Scent Booster</h2>
+      <section class="menu-card-section">
+        <b>Ingredients</b>
+        <ul>
+          <li>1 cup Epsom Salts</li>
+          <li>1/4 cup Baking Soda</li>
+          <li>10-20 drops Essential Oil of choice</li>
+        </ul>
+      </section>
+      <section class="menu-card-section">
+        <b>Directions</b>
+        <ul>
+          <li>Add 2-3 tbsp to bottom of washing machine</li>
+          <li>
+            Combo with 1-2 tbsp Dr. Bronner's Sal Suds (or other non-toxic,
+            unscented detergent)
+          </li>
+        </ul>
+      </section>
+    </section>
+
+    <section class="menu-card">
+      <h2>Multi-Purpose Spray</h2>
+      <section class="menu-card-section">
+        <b>Ingredients</b>
+        <ul>
+          <li>Rinds from 2-3 Lemons</li>
+          <li>White Vinegar</li>
+        </ul>
+      </section>
+      <section class="menu-card-section">
+        <b>Directions</b>
+        <ul>
+          <li>Remove pulp and juice of lemon leaving only the rinds</li>
+          <li>Cut into slices</li>
+          <li>Place into a spray bottle</li>
+          <li>Add vinegar til full</li>
+          <li>Let soak</li>
+          <li>Make fresh every month</li>
+          <li>
+            For use on bathroom & kitchen surfaces, mirrors & windows. **Do not
+            use on wood or stone surfaces**
+          </li>
+        </ul>
+      </section>
+    </section>
+
+    <section>
+      <img
+        src="https://ravecafe.s3.amazonaws.com/menu-002/garret.jpg"
+        alt="digital painting"
+      />
+    </section>
+    <p class="caption">
+      <b>The New Sounds</b> <br />
+      Flashe on Canvas <br />
+      Garrett Wayne Walters <br />
+      2023
+    </p>
+    <section class="essay">
+      <img
+        src="https://ravecafe.s3.amazonaws.com/menu-002/shikha.png"
+        alt="shikha"
+      />
+      <h3>
+        Shikha's
+        <a
+          href="https://www.youtube.com/playlist?list=PL7RNYybxOQBb30xGx36zJW8eSepI0HZzZ"
+          >Devotional Inspo Playlist</a
+        >
+      </h3>
+      <p class="center">by Shikha Sharma</p>
+      <p>
+        This playlist draws inspiration from the vibrant and joyful rhythms of
+        Raas Garba, a traditional musical style from Gujarat, India. It is the
+        essential thread of a cherished annual festival I attended growing up,
+        where people dance in concentric circles to live Dhol drum, fostering a
+        deep sense of shared human experience and spiritual connection.
+      </p>
+      <p>
+        Beyond Raas Garba, the curation features techno and jazz to North
+        African drum rhythms and modern electronic sounds. Also included is
+        Carnatic music from South India, in which its classical form of
+        intricate melodies, complex rhythms, and emotive improvisations speak to
+        the soul.
+      </p>
+      <p>
+        The selections remind me of the enchanting possibility of music to
+        reconnect us with the source of our being and into a shared
+        consciousness, a powerful instrument of self-preservation. Enjoy!
+      </p>
+    </section>
+    <iframe
+      width="100%"
+      height="315"
+      src="https://www.youtube.com/embed/videoseries?list=PL7RNYybxOQBb30xGx36zJW8eSepI0HZzZ"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe>
+
+    <section class="essay">
+      <img
+        src="https://ravecafe.s3.amazonaws.com/menu-002/pyramid.jpg"
+        alt="pyramid"
+      />
+      <h3>Food for Thought</h3>
+      <p class="center">by Dalton</p>
+      <p>
+        Building a relationship to our food is essential to unlocking our full
+        potential as humans. Our food choices are the main factor in how we feel
+        each day. Being intentional with our food choices is an evolving process
+        that has various levels. Do your own research and experiment to figure
+        out what works for you. What we choose to eat can enhance our lives
+        while fighting the corporate greed that is making many of us and our
+        planet sick. Here's some food for thought.
+      </p>
+      <p class="plus">+</p>
+      <p>
+        Support your local farmers by going to a farmers market in your area.
+        Creating a bond with the people who cultivate & raise the food you eat
+        is invaluable and you get the freshest food around.
+      </p>
+      <p>
+        Make reading labels a habit when you approach a new product at the
+        market. Look up anything you aren't yet familiar with. Avoid anything
+        with a long ingredient list.
+      </p>
+      <p>
+        Throw on some music and batch cook your staples for the week. Use your
+        freezer and clean as you go. Cooking your own food is essential to
+        eating well and saving money.
+      </p>
+      <p>
+        Learning knife skills will save you energy, prevent you from being cut,
+        and is the key to eating whole, unprocessed foods. Victorinox knives are
+        an affordable, quality place to start.
+      </p>
+      <p>
+        Our ancestors did not have food around all the time. We are built to
+        fast occasionally to clean out our cells. Consume electrolytes when you
+        fast such as pink salt or sugar-free electrolyte packets.
+      </p>
+      <p>
+        Pausing before eating and having a moment of gratitude for the food you
+        are consuming activates the rest & digest mode of the body aka the para-
+        sympathetic nervous system. Tuning into this mode is vital to digest
+        food properly and enhance our relationship to what we choose to consume.
+      </p>
+      <p class="minus">-</p>
+      <p>
+        Seed / “vegetable” oils are easily oxidized during manufacturing and
+        when heated up aka cooking. When these oils oxidize they make trans-fats
+        and lipid peroxides which leads to damage in DNA, proteins, and cell
+        membrane lipids throughout the body. These oils are extracted with toxic
+        solvents and are deodorized & bleached before being bottled. Deep fryer
+        oil is often reused for multiple days so avoid fried food at all costs.
+      </p>
+      <p>
+        Consuming foods and drinks with added sugar leads to inflammation,
+        obesity, and diabetes. Our brains are wired to seek out sugar in the
+        form of ripe fruit and store that sugar as fat in our bodies for times
+        of scarcity. Food companies manipulate us through this innate wiring by
+        adding sugar to their processed “foods”. Ideally sugary treats are
+        homemade and include healthy fats - it'll also taste way better than
+        anything you can buy.
+      </p>
+      <p>
+        Phytates are found in grains, nuts, & legumes. Phytates bind to
+        necessary minerals in the body which prevents absorption of those
+        minerals. They can be reduced by fermenting, sprouting, & soaking these
+        foods prior to consumption.
+      </p>
+      <p>
+        Gluten is a protein found in certain grains that triggers inflammation
+        because the body is not able to fully break it down which causes the
+        release of zonulin which leads to a leaky gut. Other grains have similar
+        but slightly less inflammatory proteins such as avenin in oats and zein
+        in corn. Gluten enzymes are available for moments when one chooses to
+        consume it.
+      </p>
+      <p>
+        Do your best in any situation and don't worry when a situation doesn't
+        align with your expectations. Stress is also a cause of inflammation.
+        I've found digestive enzymes and activated charcoal helpful tools while
+        traveling. Be where you are and enjoy the moment.
+      </p>
+      <h4>DISCLAIMER</h4>
+      <p>
+        The information provided in this zine is for educational purposes only.
+        This article is intended as a self-help tool for your own use, and
+        should not be considered or used as a substitute for medical advice,
+        diagnosis, or treatment. Use of the health-related information contained
+        is purely for the enhancement of the vibes.
+      </p>
+    </section>
+    <section class="center">
+      <img
+        src="https://ravecafe.s3.amazonaws.com/menu-002/remi.jpg"
+        alt="polaroids"
+      />
+
+      <p>
+        <b>The Four Horsemen</b>
+        <br />
+        Remington Williams
+        <br />
+        polaroids
+      </p>
+    </section>
+    <br />
+    <h3>
+      <a
+        href="https://adi.pizza/slices/free-water/"
+        target="_blank"
+        rel="norefferer nofollow"
+        >free water // liquid love
+      </a>
+    </h3>
+    <p class="center">by Aileen Amaya and Adi Dahiya</p>
+    <img
+      src="https://ravecafe.s3.amazonaws.com/menu-002/water-shrine-2.jpg"
+      alt="free water"
+    />
+
+    <section class="center">
+      <img
+        src="https://ravecafe.s3.amazonaws.com/menu-002/kelley.jpg"
+        alt="polaroids"
+      />
+
+      <p>
+        <b>Hag of the Vulva</b>
+      </p>
+      <br />
+      Kelley Donahue
+      <p>Ink on paper</p>
+      <a href="https://www.kelleydonahue.com/">kelleydonahue.com</a>
+      <br />
+      <br />
+      <p>
+        Sheila Na Gig is a pagan deity that is depicted as architectural
+        grotesques throughout Europe.
+      </p>
+    </section>
+    <img
+      src="https://ravecafe.s3.amazonaws.com/menu-002/fanny.png"
+      alt="fanny pack"
+    />
+    <img
+      src="https://ravecafe.s3.amazonaws.com/menu-002/tables.png"
+      alt="table map"
+    />
+    <NuxtLink to="/">
+      <Logo height="80" :bottom="true" />
+    </NuxtLink>
+  </section>
+</template>
+
+<style scoped>
+.essay p {
+  margin: 20px 0;
+}
+.menu-content {
+  padding-top: 5%;
+  width: 80%;
+  margin: 0 auto;
+  max-width: 700px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.plus {
+  color: #00a86b;
+  font-size: 50px;
+}
+
+.minus {
+  color: #db4437;
+  font-size: 50px;
+}
+.flex {
+  display: flex;
+  width: 100%;
+  height: 400px;
+  align-items: center;
+  justify-content: center;
+}
+
+a {
+  text-decoration: underline;
+}
+
+.header-img {
+  margin: 50px 0;
+}
+
+.mu-title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: normal;
+  font-size: 60px;
+  color: #fff;
+  width: 95%;
+  letter-spacing: 1px;
+  mix-blend-mode: difference;
+  padding: 20px;
+  text-align: center;
+}
+
+.mu-title sup {
+  font-size: 25px;
+}
+
+.threshold p {
+  margin: 5% auto;
+  max-width: 500px;
+}
+
+.center {
+  text-align: center;
+}
+
+.menu-intro {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-bottom: 10%;
+  margin-top: 4%;
+}
+
+.player {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+
+h3 {
+  font-size: 40px;
+  text-align: center;
+  margin: 10% 0 5%;
+}
+
+img {
+  width: 100%;
+  margin: 5% 0;
+  transition: opacity 5s ease-out;
+}
+
+.toggle {
+  border: 1px solid #fff;
+  border-radius: 25px;
+  margin: 20px 0 0;
+  padding: 10px;
+  width: auto;
+  overflow: visible;
+  background: transparent;
+  color: inherit;
+  font: inherit;
+  line-height: normal;
+  -webkit-font-smoothing: inherit;
+  -moz-osx-font-smoothing: inherit;
+  -webkit-appearance: none;
+}
+
+.toggle:hover {
+  background: #fff;
+  color: #1c1c1c;
+}
+
+.even-height {
+  object-fit: cover;
+  max-height: 400px;
+  width: 100%;
+}
+
+.answers {
+  padding: 0 0 0 20px;
+  margin: 20px 0;
+}
+
+.caption {
+  width: 100%;
+  margin-bottom: 20px;
+  text-align: center;
+}
+
+.specials {
+  border: 1px solid #fff;
+  padding: 20px 60px;
+  margin: 20px 0;
+  text-align: center;
+}
+
+.specials-title {
+  margin: 10px 0;
+}
+
+.specials h2 {
+  text-align: center;
+}
+
+.specials > ul > li > a {
+  text-decoration: none;
+}
+
+.specials > ul > li > a:hover {
+  text-decoration: underline;
+}
+
+.specials-footer {
+  text-align: center;
+  margin-top: 20px;
+}
+
+.menu-card {
+  border: 1px solid #fff;
+  padding: 20px 60px;
+  margin: 20px 0;
+  max-width: 500px;
+}
+
+.menu-card h2 {
+  font-size: 32px;
+}
+
+.menu-card-section {
+  margin: 20px 0;
+}
+
+.menu-card-section > ul > li:before {
+  content: '- ';
+}
+
+h3#discorebus {
+  margin: 0;
+}
+
+hr {
+  width: 100%;
+}
+
+.menu-intro {
+  max-width: 600px;
+  margin: 0 auto;
+}
+@media only screen and (max-width: 700px) {
+  .mu-title {
+    color: #fff;
+    text-decoration: none;
+    font-size: 25px;
+    padding: 10px;
+    margin-top: 10px;
+  }
+  .mu-title sup {
+    font-size: 18px;
+  }
+}
+</style>
